30e701068d7e786d25388f6b3722bf2e1ab28b1f
Erik <erik.g.ingebrigtsen@gmail.com>
2023-04-17 10:25:34 +0200

    added new more complicated graph

--- app/GraphSearch.hs
+++ app/GraphSearch.hs
@@ -5,6 +5,14 @@ module GraphSearch where
 import Data.Map as Map hiding (filter, null, drop)
 import Data.Set as Set hiding (filter, null, drop)
 
+
+{-
+    UNWEIGHTED GRAPH SEARCH
+    --BFS
+    --DFS
+    --UNWEIGHTED GRAPH REPRESENTATION
+-}
+
 data Node = Node
         {   name     :: String,
             children :: [Node],
@@ -32,7 +40,8 @@ inList a (b:bs) = a == b || inList a bs       -- skriv om med fold for effektivi
 
 
 {-
-    Breadth-First-Search
+    Breadth-First-Search:
+        always explores the oldest node added to the queue
 
     graph
     start node
@@ -54,6 +63,18 @@ bfs g start visited queue =
         queue' = tail queue''
 
 
+
+{-
+    Depth-First-Search:
+        always explores the newest node added to the queue
+
+    graph
+    start node
+    list of visited nodes
+    list of queued nodes
+    returns list of nodes in order of exploration
+
+-}
 dfs :: Graph -> Node -> [Node] -> [Node] -> [Node]
 dfs Empty _ _ _ = []
 dfs g start visited queue =
@@ -67,13 +88,13 @@ dfs g start visited queue =
         queue' = tail queue''
 
 
-
-
+{-
 a = Node "A" [b, c] (-200, -100)
 b = Node "B" [d,b] (-100, 150)
 c = Node "C" [e] (0, -200)
 d = Node "D" [c, e] (50, 100)
 e = Node "E" [] (150, 0)
+
 
 
 graph = Graph (Map.fromList [
@@ -82,3 +103,26 @@ graph = Graph (Map.fromList [
     (c, Set.fromList $ children c),
     (d, Set.fromList $ children d),
     (e, Set.fromList $ children e)])
+-}
+
+a = Node "A" [b, c, d] (-200, -100)
+b = Node "B" [e] (-100, 100)
+c = Node "C" [f, g] (-100, -50)
+d = Node "D" [h] (-20, -150)
+e = Node "E" [f] (0, 100)
+f = Node "F" [g, h] (0, 0)
+g = Node "G" [f] (0, -100)
+h = Node "H" [i] (100, -100)
+i = Node "I" [] (200, 0)
+
+graph = Graph (Map.fromList [
+    (a, Set.fromList $ children a),
+    (b, Set.fromList $ children b),
+    (c, Set.fromList $ children c),
+    (d, Set.fromList $ children d),
+    (e, Set.fromList $ children e), 
+    (f, Set.fromList $ children f),
+    (g, Set.fromList $ children g),
+    (h, Set.fromList $ children h),
+    (i, Set.fromList $ children i)])
+
